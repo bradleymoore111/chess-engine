fd0037a1f4a02634f797fa05d9c7a7a62b04d1ab
bradley <bradleym007@gmail.com>
2015-07-29 00:35:14 -0700

    added diagonals.

--- src/main/board/piece/King.java
+++ src/main/board/piece/King.java
@@ -18,26 +18,62 @@ public class King{
 		// Todo: castling
 		// Gotta add can't do it if currently checked
 
-		/* pseudocode
+		// pseudocode
 		type = board[x][y];
 
-		if(type>0){ // white
-			if(castle kingside && (pieces in between are empty)){
-				if(diagonals and horizontals are clear from bishop and rook or queen){
-					if(knight not attacking intermittent piece (specific amounts of spots you need to check)){
-						add move castle
+		if(kingSide){
+			if(type>0){ // white
+				if(board[5][0]==0&&board[6][0]==0){
+					//if(diagonals and horizontals are clear from bishop and rook or queen){
+					boolean llDiag=true,rlDiag=true,lrDiag=true,rrDiag=true;
+
+					// Left left diagonal
+					for(int i=0;i<5;i++){
+						if(board[4-i][i]==-3||board[4-i][i]==-5){
+							llDiag=false;
+							continue;
+						}else if(board[4-i][i]!=0){
+							break;
+						}
+					}
+					// Right left diagonal
+					for(int i=0;i<6;i++){
+						if(board[5-i][i]==-3||board[5-1][i]==-5){
+							rlDiag=false;
+							continue;
+						}else if(board[5-i][i]!=0){
+							break;
+						}
+					}
+					// Left right diagonal
+					if(board[6][1]==0){
+						if(board[7][2]==-3||board[7][2]==-5){
+							lrDiag=false;
+						}
+					}else if(board[6][1]==-3||board[7][2]==-5){
+						lrDiag=false;
+					}
+					// Right right diagonal
+					if(board[7][1]==-3||board[7][1]==-5){
+						rrDiag=false;
+					}
+
+					if(llDiag&&rlDiag&&lrDiag&&rrDiag){
+						// if(knight not attacking intermittent piece (specific amounts of spots you need to check)){
+						// 	add move castle
+						// }
 					}
 				}
 			}
 			if(castle queenside && pieces in between are empty){
 				// same chain as above. Could compress entire thing into one long logical statement, will probably do that
 			}
-		}else{ // black
+		}
+		if(queenSide){
 			// Same chains as above, except black side and black pieces
 		}
 		// later on, during the actual moving of pieces, check every time if pawn or king (cause special rules)
 		// if king, check if more than one move horizontally, if so, move rook as well
-		 */
 
 		return moves;
 	}
